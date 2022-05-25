74402746737005c3813f1ab4c47e563d65bae815
Marina Glancy <marina.glancy@gmail.com>
2022-05-25 22:19:17 +0200

    Restored deleted format.js, it is still needed

---
+++ format.js
@@ -0,0 +1,28 @@
+// Javascript functions for Flexible sections course format.
+
+// This is no longer used but there are errors in console if this file is missing.
+
+M.course = M.course || {};
+
+M.course.format = M.course.format || {};
+
+/**
+ * Get sections config for this format.
+ *
+ * The section structure is:
+ * <ul class="flexsections">
+ *  <li class="section">...</li>
+ *  <li class="section">...</li>
+ *   ...
+ * </ul>
+ *
+ * @return {object} section list configuration
+ */
+M.course.format.get_config = function() {
+    return {
+        container_node: 'ul',
+        container_class: 'flexsections',
+        section_node: 'li',
+        section_class: 'section'
+    };
+};
--- format.php
+++ format.php
@@ -43,3 +43,6 @@ if (!empty($displaysection)) {
 $outputclass = $format->get_output_classname('content');
 $widget = new $outputclass($format);
 echo $renderer->render($widget);
+
+// Include course format js module.
+$PAGE->requires->js('/course/format/flexsections/format.js');
