40428a4e299c81d8656eb27dd48d0dac55e699df
Marina Glancy <marina.glancy@gmail.com>
2022-06-19 17:33:39 +0200

    New release 4.0.1

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022052200;             // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022041900.00;          // Requires this Moodle 4.0 or above.
-$plugin->release   = "4.0.1dev";
-$plugin->maturity  = MATURITY_BETA;
+$plugin->version   = 2022061900;             // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022041900.00;          // Requires Moodle 4.0 or above.
+$plugin->release   = "4.0.1";
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'format_flexsections';  // Full name of the plugin (used for diagnostics).
 $plugin->supported = [400, 400];
