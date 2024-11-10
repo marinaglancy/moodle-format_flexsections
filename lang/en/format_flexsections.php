a919b879fd2a11f5bf9b195c3bb2b17793b0c214
giuliovale <giulio.valentino@gmail.com>
2024-11-10 14:45:30 +0100

    Add cssflex

--- lang/en/format_flexsections.php
+++ lang/en/format_flexsections.php
@@ -58,6 +58,7 @@ $string['movesectiontotheend'] = 'To the end';
 $string['newsectionname'] = 'New name for section {$a}';
 $string['page-course-view-flexsections'] = 'Any course main page in Flexible sections format';
 $string['page-course-view-flexsections-x'] = 'Any course page in Flexible sections format';
+$string['plugin_description'] = 'The course is divided into sections, each section can be displayed on the course page or as a link. Sections can have subsections.';
 $string['pluginname'] = 'Flexible sections format';
 $string['privacy:metadata'] = 'The Flexible sections format plugin does not store any personal data.';
 $string['section0name'] = 'General';
@@ -69,6 +70,8 @@ $string['showsection0title'] = 'Show top section title';
 $string['showsection0title_help'] = 'When enabled, the general section will have a title and will be collapsible, same as it behaves in the Topics format.';
 $string['showsection0titledefault'] = 'Show top section title by default';
 $string['showsection0titledefaultdesc'] = 'This defines default setting that will be used for new and existing courses, it can be changed for individual courses in their settings.';
+$string['cssflex'] ='CSS Class for this section';
+
 
 // Deprecated since 4.1.
 $string['addsubsectionfor'] = 'Add subsection for \'{$a}\'';
