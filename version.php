68d1f1a134326cf3fd11d8eb63602e9efe8d535b
Marina Glancy <marina.glancy@gmail.com>
2026-03-28 16:37:29 +0000

    Add tests on 5.2

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->requires  = 2025041400.00;          // Requires Moodle 5.0 or above.
 $plugin->release   = "5.0.2";
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'format_flexsections';  // Full name of the plugin (used for diagnostics).
-$plugin->supported = [500, 501];
+$plugin->supported = [500, 502];
